076751ed0a08754aa00794f9ed0ad299507494cd
Endum Boris <endumboris7@gmail.com>
2026-06-09 12:54:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -65,6 +65,34 @@ const typed = new Typed('.multiple-text', {
     loop: true
 
 });
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach(counter => {
+
+    const updateCounter = () => {
+
+        const target = +counter.getAttribute('data-target');
+        const count = +counter.innerText;
+
+        const increment = target / 100;
+
+        if(count < target){
+
+            counter.innerText = Math.ceil(count + increment);
+
+            setTimeout(updateCounter, 20);
+
+        }else{
+
+            counter.innerText = target;
+
+        }
+
+    }
+
+    updateCounter();
+
+});
 // CONTACT FORM VALIDATION
 
 const form = document.getElementById("contact-form");
