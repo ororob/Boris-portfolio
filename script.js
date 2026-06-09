a27e0ba8b2950633fda90b92c0851af34f00325c
Endum Boris <endumboris7@gmail.com>
2026-06-09 14:06:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,50 @@
 // js/script.js
+const counters = document.querySelectorAll('.counter');
 
+const observer = new IntersectionObserver((entries)=>{
+
+    entries.forEach(entry=>{
+
+        if(entry.isIntersecting){
+
+            const counter = entry.target;
+            const target = +counter.dataset.target;
+
+            let count = 0;
+
+            const update = () => {
+
+                count += Math.ceil(target / 50);
+
+                if(count < target){
+
+                    counter.innerText = count;
+
+                    requestAnimationFrame(update);
+
+                }else{
+
+                    counter.innerText = target;
+
+                }
+
+            };
+
+            update();
+
+            observer.unobserve(counter);
+
+        }
+
+    });
+
+});
+
+counters.forEach(counter=>{
+
+    observer.observe(counter);
+
+});
 // MOBILE MENU
 
 let menuIcon = document.querySelector('#menu-icon');
